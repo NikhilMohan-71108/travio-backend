b645180bd9d2c2b58ed56e509bfcd12812a7b4d2
Nikhil Mohan <nikhilmohan.11@gmail.com>
2026-05-05 10:29:38 +0530

    initial commit

---
+++ src/main/java/com/travio/travio_backend/TravioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.travio.travio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravioBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravioBackendApplication.class, args);
+    }
+
+}
